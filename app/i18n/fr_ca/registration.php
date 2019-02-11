12829c233d7e581cb21c7c265fe9fd6f65d740dd
Xavi Nguyen <vphat28@gmail.com>
2019-02-11 20:55:39 +0700

    backup

---
+++ app/i18n/fr_ca/registration.php
@@ -0,0 +1,11 @@
+<?php
+
+/**
+ * Magento2 French Language Pack (fr_CA)
+ *
+ */
+\Magento\Framework\Component\ComponentRegistrar::register(
+    \Magento\Framework\Component\ComponentRegistrar::LANGUAGE,
+    'moneris_fr_ca',
+    __DIR__
+);
