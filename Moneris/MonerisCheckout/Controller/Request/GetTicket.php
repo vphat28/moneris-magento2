6db2c2736ed852bfe2e0409f23b047197a04eeea
Xavi Nguyen <vphat28@gmail.com>
2021-01-19 22:19:49 +0700

    Fix pro code

--- Moneris/MonerisCheckout/Controller/Request/GetTicket.php
+++ Moneris/MonerisCheckout/Controller/Request/GetTicket.php
@@ -181,7 +181,7 @@ class Getticket extends Action
 
 
                 $itemDataToSend->description = $this->stripBadCharacters($product->getName());
-                $itemDataToSend->product_code = $this->stripBadCharacters($product->getSku());
+                $itemDataToSend->product_code = $this->stripBadCharacters($product->getSku(), 50);
                 $itemDataToSend->unit_cost = $this->formatPrice($item->getPrice());
                 $itemDataToSend->quantity = $item->getQty();
 
@@ -308,10 +308,59 @@ class Getticket extends Action
         return $result;
     }
 
-    protected function stripBadCharacters($str)
-    {
-        $str = preg_replace('/[\\\<\>\$\%\=\?\^\{\}\[\]]/', '', $str);
 
-        return $str;
+
+    /**
+     * Checks to see if a string is utf8 encoded.
+     *
+     * NOTE: This function checks for 5-Byte sequences, UTF8
+     *       has Bytes Sequences with a maximum length of 4.
+     *
+     * @author bmorel at ssi dot fr (modified)
+     * @since 1.2.1
+     *
+     * @param string $str The string to be checked
+     * @return bool True if $str fits a UTF-8 model, false otherwise.
+     */
+    function seems_utf8( $str ) {
+        mbstring_binary_safe_encoding();
+        $length = strlen( $str );
+        reset_mbstring_encoding();
+        for ( $i = 0; $i < $length; $i++ ) {
+            $c = ord( $str[ $i ] );
+            if ( $c < 0x80 ) {
+                $n = 0; // 0bbbbbbb
+            } elseif ( ( $c & 0xE0 ) == 0xC0 ) {
+                $n = 1; // 110bbbbb
+            } elseif ( ( $c & 0xF0 ) == 0xE0 ) {
+                $n = 2; // 1110bbbb
+            } elseif ( ( $c & 0xF8 ) == 0xF0 ) {
+                $n = 3; // 11110bbb
+            } elseif ( ( $c & 0xFC ) == 0xF8 ) {
+                $n = 4; // 111110bb
+            } elseif ( ( $c & 0xFE ) == 0xFC ) {
+                $n = 5; // 1111110b
+            } else {
+                return false; // Does not match any model.
+            }
+            for ( $j = 0; $j < $n; $j++ ) { // n bytes matching 10bbbbbb follow ?
+                if ( ( ++$i == $length ) || ( ( ord( $str[ $i ] ) & 0xC0 ) != 0x80 ) ) {
+                    return false;
+                }
+            }
+        }
+        return true;
+    }
+
+    protected function stripBadCharacters($str, $leng = 0)
+    {
+        $str = stripslashes($str);
+        $str = preg_replace("/[\\\<\>\$\%\=\?\^\{\}\'\"\/\[\]]/", '', $str);
+
+        if (!empty($leng)) {
+            return substr($str, 0, $leng);
+        }
+
+        return ($str);
     }
 }
